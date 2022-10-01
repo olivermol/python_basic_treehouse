da28633b71ce086f81dd37b929b3999b0adfc157
olivermoln <dris.dev@icloud.com>
2022-10-01 12:41:39 +0200

    modifided "// 4"

--- notifications.py
+++ notifications.py
@@ -1,7 +1,7 @@
 def yell(text):
     text = text.upper()
     number_of_characters = len(text)
-    result = text + "!" * (number_of_characters // 2)
+    result = text + "!" * (number_of_characters // 4)
     print(result)
 
 yell("You are doing great")
