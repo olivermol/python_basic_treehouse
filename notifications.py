1e230993fe637743f90fddbc97827dc08a04df0a
olivermoln <dris.dev@icloud.com>
2022-10-01 10:59:37 +0200

    Notification deleted --> Notifications is valid

--- notifications.py
+++ notifications.py
@@ -1,17 +1,9 @@
-praise = "You are doing great"
-praise = praise.upper()
-number_of_characters = len(praise)
-result = praise + "!" * (number_of_characters // 2)
-print(result)
+def yell(text):
+    text = text.upper()
+    number_of_characters = len(text)
+    result = text + "!" * (number_of_characters // 2)
+    print(result)
 
-advice = "Don't forget to ask for help"
-adivce = advice.upper()
-number_of_characters = len(advice)
-result = advice + "!" * number_of_characters
-print(result)
-
-advice2 = "Don't repeat yourself. Keep things DRY"
-adivce2 = advice2.upper()
-number_of_characters = len(advice2)
-result = advice2 + "!" * number_of_characters
-print(result)
+yell("You are doing great")
+yell("Don't forget to ask for help")
+yell("Don't repeat yourself. Keep things DRY")
