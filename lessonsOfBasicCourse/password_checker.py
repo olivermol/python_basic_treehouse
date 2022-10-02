fa0d62a0666813c1597a7426388579f00f88fbe5
olivermol <111631561+olivermol@users.noreply.github.com>
2022-10-02 22:13:14 +0200

    corrected

--- lessonsOfBasicCourse/password_checker.py
+++ lessonsOfBasicCourse/password_checker.py
@@ -9,4 +9,4 @@ while password != MASTER_PASSWORD:
         sys.exit("Too many invalid password attempts")
     password = input("Invalid password, try again: \n")
     attempt_count += 1
-print("Welcom to secret village")
+print("Welcome to secret village")
